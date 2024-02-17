7a5b3855a9fb58d237de2c8295a385878ac838ff
Morteza <asgharzadehmorteza@gmail.com>
2024-02-17 19:54:03 +0330

    Edit Fetch Options

--- app/search-result/page.tsx
+++ app/search-result/page.tsx
@@ -21,7 +21,18 @@ const SearchResult = () => {
 
   const FETCH_API = async (endPoint: string) => {
     setRankedResults(undefined);
-    const response = await fetch(endPoint);
+    const response = await fetch(endPoint, {
+      method: "GET", 
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    });
     if (response.ok) {
       const finalResult = await response.json();
       setRankedResults(finalResult);
